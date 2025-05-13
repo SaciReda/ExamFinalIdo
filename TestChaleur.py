bec3b954dfad9ee20a174a580f3920ad718905ac
SaciReda <reda211@outlook.com>
2025-05-13 09:51:34 -0400

    Manque le web les leds et voila

--- TestChaleur.py
+++ TestChaleur.py
@@ -1,69 +1,33 @@
-
-
-
 from pigpio_dht import DHT11
-
+import pigpio
+import threading
 import time
- 
 gpio = 21
- 
+btn = 26
 sensor = DHT11(gpio)
+pi = pigpio.pi()
 
-while True:
-
-    result = sensor.read()
-    if result['valid']==True:
-        print("Température :", result['temp_c'], "°C ",result['valid'])
-    else:
-        continue
-
-    time.sleep(3)
+def envoieInfo():
+   result = sensor.read()
+   if result['valid'] == True:
+       print("Température :", result['temp_c'], "°C", result['valid'])
+   else:
+       print("rien envoyé")
 
 
+def click():
+   pi.set_mode(btn, pigpio.INPUT)
+   pi.set_pull_up_down(btn, pigpio.PUD_DOWN)
+   bouton = pi.read(btn)
+   if bouton == 0:
+        envoieInfo()
+        print("envoyé par bouton")
+        time.sleep(0.5)
 
- 
-
-
-
-
-from pigpio_dht import DHT11, DHT22
-import time
-import paho.mqtt.client as pmc
-
-BROKER = "mqttbroker.lan"
-PORT = 1883
-TOPIC = "final/pira/T"
-gpio = 21 
-sensor = DHT11(gpio)        
-result = sensor.read()
-print(result)
-def connexion(client, userdata, flags, code, properties):
-    if code == 0:
-        print("Connecté")
-    else:
-        print("Erreur code %d\n", result)
-
-client = pmc.Client(pmc.CallbackAPIVersion.VERSION2)
-client.on_connect = connexion
-
-client.connect(BROKER,PORT)
-client.publish(TOPIC,result)
-client.disconnect()
-
-from pigpio_dht import DHT11
-
-import time
- 
-gpio = 21
- 
-sensor = DHT11(gpio)
-
-while True:
-
-    result = sensor.read()
-   
-    print("Température :", result['temp_c'], "°C")
-    
-
-    time.sleep(3)
-
+try:
+   thread_click = threading.Thread(target=click, daemon=True)
+   thread_click.start()
+   while True:
+       time.sleep(2)
+except KeyboardInterrupt:
+   print("éteint manuellement")
